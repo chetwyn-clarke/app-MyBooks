30c2a07b45b7dfcdb844f411b100555c1be75e35
Chetwyn <chetwynclarke@Chetwyn-Clarkes-MacBook.local>
2017-12-11 18:08:38 -0500

    Added Book class

---
+++ My Books/My Books/Model/Book.swift
@@ -0,0 +1,18 @@
+//
+//  Book.swift
+//  My Books
+//
+//  Created by Chetwyn on 12/11/17.
+//  Copyright © 2017 Clarke Enterprises. All rights reserved.
+//
+
+import Foundation
+
+class Book {
+    
+    var name: String = ""
+    var firstAuthor: String?
+    var secondAuthor: String?
+    var thirdAuthor: String?
+    
+}
